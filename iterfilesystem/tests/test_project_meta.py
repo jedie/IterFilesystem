e46f396f65b4331ffe30f091277449b03bb1b259
JensDiemer <git@jensdiemer.de>
2019-09-15 15:05:02 +0200

    pipenv check work-a-round, see:
    https://travis-ci.org/jedie/IterFilesystem/jobs/585193735
    https://ci.appveyor.com/project/jedie/iterfilesystem/builds/27421121/job/66qn32v2qrcyuoi9

--- iterfilesystem/tests/test_project_meta.py
+++ iterfilesystem/tests/test_project_meta.py
@@ -1,10 +1,10 @@
 """
     Some project meta tests
 """
+import subprocess
 
 import pytest
 from flake8.main.cli import main
-from pipenv.core import do_check
 
 
 def test_flake8():
@@ -16,13 +16,12 @@ def test_flake8():
     assert pytest_wrapped_e.value.code == 0
 
 
-def test_pipenv_check(capsys):
-    do_check()
-
-    captured = capsys.readouterr()
-    print(captured.out)
-    assert 'Passed!' in captured.out
-    assert 'All good!' in captured.out
-
-    print(captured.err)
-    assert captured.err == ""
+def test_pipenv_check():
+    # we can just call pipenv.core.do_check()
+    # But this will fail on TravisCI and appveyor.com
+    #
+    # work-a-round:
+    output = subprocess.check_output(["pipenv", "check"], universal_newlines=True, timeout=10)
+    print(output)
+    assert 'Passed!' in output
+    assert 'All good!' in output
