88e2eb8831a76c0baa1fbe1b13385b698f751daa
JensDiemer <git@jensdiemer.de>
2019-09-15 15:27:01 +0200

    wip CI setup

--- iterfilesystem/utils.py
+++ iterfilesystem/utils.py
@@ -6,6 +6,9 @@ def left_shorten(text, width=30, placeholder='...'):
     """
     >>> left_shorten('alongstring', width=8, placeholder='..')
     '..string'
+
+    >>> left_shorten('1234567890', width=12)
+    '  1234567890'
     """
     if len(text) > width:
         width -= len(placeholder)
