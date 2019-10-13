0731b7895886807a1972218998bc85a9a9aa5ec8
JensDiemer <git@jensdiemer.de>
2019-10-13 12:41:32 +0200

    code cleanup

--- iterfilesystem/tests/test_project_meta.py
+++ iterfilesystem/tests/test_project_meta.py
@@ -5,12 +5,12 @@ import subprocess
 import sys
 
 import pytest
-from flake8.main.cli import main
+from flake8.main.cli import main as flake8cli
 
 
 def test_flake8():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        main([
+        flake8cli([
             # "--verbose"
         ])
 
@@ -22,7 +22,11 @@ def test_pipenv_check():
     # But this will fail on TravisCI and appveyor.com
     #
     # work-a-round:
-    output = subprocess.check_output([sys.executable, "-m", "pipenv", "check"], universal_newlines=True, timeout=20)
+    output = subprocess.check_output(
+        [sys.executable, "-m", "pipenv", "check"],
+        universal_newlines=True,
+        timeout=20
+    )
     print(output)
     assert 'Passed!' in output
     assert 'All good!' in output
