421a38bef53b73b96c702f50eb9e0cf253084f24
JensDiemer <git@jensdiemer.de>
2020-02-02 16:30:08 +0100

    use poetry and modernize project setup

---
+++ iterfilesystem/publish.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from poetry_publish.publish import poetry_publish
+from poetry_publish.utils.subprocess_utils import verbose_check_call
+
+# https://github.com/jedie/IterFilesystem
+import iterfilesystem
+
+
+def publish():
+    """
+        Publish to PyPi
+        Call this via:
+            $ poetry run publish
+    """
+    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
+
+    poetry_publish(
+        package_root=Path(iterfilesystem.__file__).parent.parent,
+        version=iterfilesystem.__version__,
+        creole_readme=True  # don't publish if README.rst is not up-to-date
+    )
