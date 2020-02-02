6e97a3f361c7002987667aee3de10b84562f120a
JensDiemer <git@jensdiemer.de>
2020-02-02 17:13:24 +0100

    use poetry_publish.tests.test_project_setup see:
    https://github.com/jedie/poetry-publish/pull/3/commits/0f5994c2cda53d24838a738ec4b9e61e0c51b074#diff-bcda810eed6126543d09305bcaa20b94

--- iterfilesystem/tests/test_project_setup.py
+++ iterfilesystem/tests/test_project_setup.py
@@ -3,55 +3,23 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-import os
-import shutil
-import subprocess
+
 from pathlib import Path
+
+from poetry_publish.tests import test_project_setup
 
 # IterFilesystem
 import iterfilesystem
-from iterfilesystem import __version__
 
 PACKAGE_ROOT = Path(iterfilesystem.__file__).parent.parent
 
 
-def assert_file_contains_string(file_path, string):
-    with file_path.open('r') as f:
-        for line in f:
-            if string in line:
-                return
-    raise AssertionError(f'File {file_path} does not contain {string!r} !')
-
-
 def test_version():
-    if 'dev' not in __version__ and 'rc' not in __version__:
-        version_string = f'v{__version__}'
-
-        assert_file_contains_string(
-            file_path=Path(PACKAGE_ROOT, 'README.creole'),
-            string=version_string
-        )
-
-        assert_file_contains_string(
-            file_path=Path(PACKAGE_ROOT, 'README.rst'),
-            string=version_string
-        )
-
-    assert_file_contains_string(
-        file_path=Path(PACKAGE_ROOT, 'pyproject.toml'),
-        string=f'version = "{__version__}"'
+    test_project_setup.test_version(
+        package_root=PACKAGE_ROOT,
+        version=iterfilesystem.__version__
     )
 
 
 def test_poetry_check():
-    poerty_bin = shutil.which('poetry')
-
-    output = subprocess.check_output(
-        [poerty_bin, 'check'],
-        universal_newlines=True,
-        env=os.environ,
-        stderr=subprocess.STDOUT,
-        cwd=str(PACKAGE_ROOT),
-    )
-    print(output)
-    assert output == 'All set!\n'
+    test_project_setup.test_poetry_check(package_root=PACKAGE_ROOT)
